97141962e14051b3fa73caf9ced917a37ceda81d
johnny <jbui923@gmail.com>
2021-04-12 10:35:44 -0700

    starter

---
+++ starter/server.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const fs = require('fs');
+
+const { Player } = require('./game/class/player');
+const { World } = require('./game/class/world');
+
+const worldData = require('./game/data/basic-world-data');
+
+let player;
+let world = new World();
+world.loadWorld(worldData);
+
+const server = http.createServer((req, res) => {
+  let reqBody = '';
+  
+  req.on('data', (data) => {
+    reqBody += data;
+  });
+
+  req.on('end', () => {
+    if (reqBody) {
+      req.body = reqBody
+        .split("&")
+        .map((keyValuePair) => keyValuePair.split("="))
+        .map(([key, value]) => [key, value.replace(/\+/g, " ")])
+        .map(([key, value]) => [key, decodeURIComponent(value)])
+        .reduce((acc, [key, value]) => {
+          acc[key] = value;
+          return acc;
+        }, {});
+    }
+
+  });
+});
+
+const port = 5000;
+
+server.listen(port, () => console.log('Server is listening on port', port));
